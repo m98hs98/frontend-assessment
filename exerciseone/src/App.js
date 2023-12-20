39a23cc0d4165eca96faeffebceb35853164173e
Mahsa <mahsahaddadi19@gmail.com>
2023-12-20 15:32:36 +1100

    changes

--- exerciseone/src/App.js
+++ exerciseone/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './component/Header/Header';
 import Card from './component/Card/Card';
+import image01 from './assets/01.jpg';
 
 
 
@@ -14,13 +15,25 @@ const App = () => {
       <div className="container">
         <div className='row'>
           <div className='col'>
-            <Card />
+            <Card 
+                image={image01}
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat."
+              />
           </div>
           <div className='col'>
-            <Card />
+            <Card 
+                image={image01}
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat."
+              />
           </div>
           <div className='col'>
-            <Card />
+            <Card 
+                image={image01}
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat."
+              />
           </div>
         </div>
       </div>
--- exerciseone/src/component/Card/Card.js
+++ exerciseone/src/component/Card/Card.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Card.css';
-import image1 from '../../assets/01.jpg';
 
 
-const Card = () => {
+
+const Card = (props) => {
     return (
         <div className="card custom__card">
             <div className='card__img__container'>
-                <img src={image1} className="card__img__top" alt="..." />
+                <img src={props.image} className="card__img__top" alt="..." />
                 <p className='card__footer'>
                     <small className='footer__copyright'>Powered by HTML.COM</small>
                 </p>
             </div>
             <div className="card__body">
-                <p className="card__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-aliquip ex ea commodo consequat.</p>
+                <p className="card__text">{props.text}</p>
                 <div className='card__button__container'>
                     <a href="/" className="btn btn--primary">READ MORE</a>
                 </div>
